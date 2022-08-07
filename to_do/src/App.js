a91693c9b240fab3a5a715b11c8dc9b10e4f0c53
Danielecarn <dannygoncalves41@gmail.com>
2022-08-07 18:14:23 -0300

    Estrutura do formulário

--- to_do/src/App.js
+++ to_do/src/App.js
@@ -11,13 +11,54 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done:false,
+    }
+    //Envio para api
+    console.log(todo);
+
+    setTitle("");
+    setTime("");
+  }
+
   return (
     <div className="App">
       <div className="todo-header">
         <h1>React Todo</h1>
       </div>
       <div className="form-todo">
-        <p>Formulario</p>
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input 
+              type="text" 
+              name="title" 
+              placeholder="Título da tarefa" 
+              onChange={(e)=> setTitle(e.target.value)}
+              value={title || ""}
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input 
+              type="text" 
+              name="time" 
+              placeholder="Tempo estimado (em horas)" 
+              onChange={(e)=> setTime(e.target.value)}
+              value={time || ""}
+              required
+            />
+          </div>
+          <input type="submit" value="Criar Tarefa"/>
+        </form>
       </div>
       <div className="list-todo">
         <h2>Lista de tarefas:</h2>
